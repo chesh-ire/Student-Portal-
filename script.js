163d23a53156e1e7e69225f161708c257bb5b747
Cheshire <126002367+SSHAARVARI@users.noreply.github.com>
2024-06-11 11:40:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example: Add event listener to the "Set Goals" button
+const setGoalsButton = document.querySelector('.profile button');
+
+setGoalsButton.addEventListener('click', () => {
+  // Logic to handle clicking the button, maybe open a modal or redirect
+  console.log('Set Goals button clicked!');
+});
